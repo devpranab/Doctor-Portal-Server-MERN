4d65f1d6200b18fd8c51e37579e4a1469b4c291e
pranav s <dev.pranabsk@gmail.com>
2022-12-15 11:32:02 +0530

    Create Server to store

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const MongoClient = require('mongodb').MongoClient;
-require('dotenv').config();
+require('dotenv').config()
 
 const app = express();
 
 app.use(bodyParser.json())
 app.use(cors());
 
-const port = 5000
+const port = 5000;
 
 app.get('/', (req, res) => {
     res.send("Hello from db working!");
@@ -18,21 +18,24 @@ app.get('/', (req, res) => {
 
 //Mongo DB Start
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.l2ldzsy.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
-
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+// perform actions on the collection object
+// Add Appointment from here and Sending DataBase
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true  });
 client.connect(err => {
-  const appointmentCollection = client.db("Doctor-Portal").collection("appointments");
-  // perform actions on the collection object
-  // Add Appointment from here and Sending DataBase
-    app.post('/addAppointment', (req, res) => {
-        const appointment = req.body;
-        appointmentCollection.insertOne(appointment)
-        .then(result => {
-            console.log('Appointment Added ', result);
-            res.send(result.insertedCount);
-        });
+  const appointmentCollection = client.db("doctorsPortal").collection("appointments");
+
+  app.post('/addAppointment', (req,res)=> {
+      const appointment = req.body;
+      console.log(appointment);
+      appointmentCollection.insertOne(appointment)
+      .then(result => {
+          res.send(result.insertedCount > 0)
+      })
     });
 });
 //Mongo DB End
 
-app.listen(process.env.PORT || port);
+
+app.listen(process.env.PORT || port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+});
